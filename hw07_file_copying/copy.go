ff9f679eaadea7a129801d11bdfe423e03205e74
adrianoff <adrianov87@gmail.com>
2024-02-03 17:21:28 +0300

    hw07 Copy completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,57 +2,92 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"io/fs"
 	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSameFiles             = errors.New("source and destination files are the same")
+	ErrWrongParams           = errors.New("offset and limit should be positive integers")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	inFile, err := os.Open(fromPath)
+func Copy(sourcePath, destinationPath string, offset, limit int64) error {
+	// Check offset and limit should be positive integers
+	if offset < 0 || limit < 0 {
+		return ErrWrongParams
+	}
+
+	var sourceFile, destinationFile *os.File
+	var sourceFileInfo, destinationFileInfo os.FileInfo
+	var err error
+
+	// Open source file
+	if sourceFile, err = os.Open(sourcePath); err != nil {
+		return fmt.Errorf("open source file error: %w", err)
+	}
+	defer sourceFile.Close()
+
+	// Get source file info and check file is not device
+	if sourceFileInfo, err = sourceFile.Stat(); err != nil {
+		return fmt.Errorf("retrieving source file info error: %w", err)
+	}
+	if isSpecificMode(sourceFileInfo.Mode(), fs.ModeDevice) {
+		return ErrUnsupportedFile
+	}
+
+	// Check offset > filesize
+	filesize := sourceFileInfo.Size()
+	if offset > filesize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Open destination file
+	if destinationFile, err = os.Create(destinationPath); err != nil {
+		return fmt.Errorf("open destination file error: %w", err)
+	}
+	defer destinationFile.Close()
+
+	// Check destination file is not the same as source file
+	if destinationFileInfo, err = destinationFile.Stat(); err != nil {
+		return fmt.Errorf("retrieving destination file info error: %w", err)
+	}
+	if os.SameFile(sourceFileInfo, destinationFileInfo) {
+		return ErrSameFiles
+	}
+
+	if offset != 0 { // Let`s avoid Seek if offset is 0. Cursor is already on 0.
+		_, err = sourceFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	if limit == 0 {
+		limit = filesize - offset // If limit == 0 than copy 'filesize-offset' bytes max
+	} else if offset+limit > filesize {
+		limit = filesize - offset // if offset+limit > filesize than copy 'filesize - offset' bytes max
+	}
+	reader := io.LimitReader(sourceFile, limit)
+	writer := destinationFile
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(reader)
+
+	_, err = io.Copy(writer, barReader)
 	if err != nil {
 		return err
 	}
 
-	data := make([]byte, 10)
+	bar.Finish()
 
-	for {
-		n, err := inFile.ReadAt(data, offset)
-		if err != nil {
-			return err
-		}
-		offset = offset + int64(n)
-	}
-
-	//read, err = inFile.ReadAt(data, offset)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//count := 100000
-	//
-	//// create and start new bar
-	//bar := pb.StartNew(count)
-	//
-	//// start bar from 'default' template
-	//// bar := pb.Default.Start(count)
-	//
-	//// start bar from 'simple' template
-	//// bar := pb.Simple.Start(count)
-	//
-	//// start bar from 'full' template
-	//// bar := pb.Full.Start(count)
-	//
-	//for i := 0; i < count; i++ {
-	//	bar.Increment()
-	//	time.Sleep(time.Millisecond)
-	//}
-	//
-	//// finish bar
-	//bar.Finish()
-
-	// Place your code here.
 	return nil
+}
+
+func isSpecificMode(mode, targetMode os.FileMode) bool {
+	return mode&targetMode == targetMode
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"flag"
+)
+
+var (
+	from, to      string
+	limit, offset int64
+)
+
+func init() {
+	flag.StringVar(&from, "from", "", "file to read from")
+	flag.StringVar(&to, "to", "", "file to write to")
+	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
+	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+}
+
 func main() {
-	Copy("testdata/input.txt", "", 0, 10)
+	flag.Parse()
+	if err := Copy(from, to, offset, limit); err != nil {
+		panic(err)
+	}
 }
