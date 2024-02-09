a27abea93d0548337c6b58fcc822b9bf49b6e4cc
adrianoff <adrianov87@gmail.com>
2024-02-10 00:36:10 +0300

    envdir draft

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +17,14 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		fmt.Println(e.Name())
+	}
+
 	return nil, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	var path, command string
+
+	path = os.Args[1]
+	command = os.Args[2]
+	others := os.Args[3:]
+
+	_, _ = ReadDir(path)
+	fmt.Println(path, command, others)
+
+	//cmd := exec.Command("git", "commit", "-am", "fix")
 }
