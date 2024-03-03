d3c7c9d6a290eaa39bc99a20d225fffb0fc9af67
adrianoff <adrianov87@gmail.com>
2024-03-03 23:03:15 +0300

    hw09_struct_validator fix linter

--- hw09_struct_validator/validators/inValidator.go
+++ hw09_struct_validator/validators/inValidator.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-//gocognit:ignore
+//nolint:gocognit
 func InValidator(inValues string, v reflect.Value) error {
 	IntKinds := []reflect.Kind{reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64}
 	switch {
